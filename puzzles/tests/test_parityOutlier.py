31e65ddee43cce89cdfc14cb70b542fa78ba91f7
MalsR <malinda_rajapakse@hotmail.com>
2016-09-26 22:28:35 +0100

    Correct test names

--- puzzles/tests/test_parityOutlier.py
+++ puzzles/tests/test_parityOutlier.py
@@ -5,13 +5,13 @@ from puzzles.puzzle.ParityOutlier import ParityOutlier
 
 class TestParityOutlier(TestCase):
 
-    def test_returns_only_odd_number_when_all_other_numbers_are_even(self):
+    def test_returns_odd_number_when_all_other_numbers_are_even(self):
         parity_outlier = ParityOutlier()
         numbers_to_find = [2, 4, 3, 8, 100, 200]
 
         self.assertEqual(3, parity_outlier.find(numbers_to_find))
 
-    def test_returns_odd_number_when_other_numbers_are_even(self):
+    def test_returns_even_number_when_other_numbers_are_odd(self):
         parity_outlier = ParityOutlier()
         numbers_to_find = [0, 1, 2, 4, 6, 88, 102]
 
