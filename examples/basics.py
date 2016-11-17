095caf9c91fde32740e1b5598173c97b7ce25bfd
MalsR <malinda_rajapakse@hotmail.com>
2016-11-17 21:21:54 +0000

    Basic python learning

---
+++ examples/basics.py
@@ -0,0 +1,15 @@
+# normal floor division
+normal_division = 15 / 2
+print(normal_division)
+
+# floor division or integer division
+integer_division = 15 // 2
+print(integer_division)
+
+# print a length of string
+hello_world = "Hello World!"
+print(len(hello_world))
+
+# using slice to access parts of string
+slice_example = "I'm going to be sliced!"
+print(slice_example[4:7])
