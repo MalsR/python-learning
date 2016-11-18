8ae15f0503935579618cc1fc543f10093a979b2c
MalsR <malinda_rajapakse@hotmail.com>
2016-11-18 23:03:39 +0000

    Some python basics

--- examples/basics.py
+++ examples/basics.py
@@ -9,7 +9,20 @@ print(integer_division)
 # print a length of string
 hello_world = "Hello World!"
 print(len(hello_world))
+print(hello_world[1] + hello_world[4])
 
 # using slice to access parts of string
 slice_example = "I'm going to be sliced!"
 print(slice_example[4:7])
+print(slice_example[:6])
+print(slice_example[1:])
+
+# boolean operators and/or
+# input() for user prompt
+day = input("What day is it today? ")
+print('You\'ve said:', day)
+
+# convert String to int
+age = input("How old are you? ")
+age_as_int = int(age)
+print('You are :', age)
