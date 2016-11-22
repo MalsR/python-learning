371df2ab6ebdb8baeaa39c35874afbf27d3eff18
MalsR <malinda_rajapakse@hotmail.com>
2016-11-22 20:50:21 +0000

    Update to sleep_in

--- puzzles/codingbat/sleep_in.py
+++ puzzles/codingbat/sleep_in.py
@@ -1,5 +1,6 @@
 # The parameter weekday is True if it is a weekday,
-# and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+# and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation.
+# Return True if we sleep in.
 
 # sleep_in(False, False) → True
 # sleep_in(True, False) → False
@@ -9,9 +10,7 @@
 class SleepIn:
 
     def sleep_in(self, weekday, vacation):
-        if weekday is False:
-            return True
-        elif vacation:
+        if weekday is False or vacation:
             return True
         else:
             return False
