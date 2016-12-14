aa6598ccc848ac3dd4c96a3f4a32633db6cd0ee9
MalsR <malinda_rajapakse@hotmail.com>
2016-12-14 22:25:10 +0000

    Add quit option to guessing game

--- examples/guess_the_number.py
+++ examples/guess_the_number.py
@@ -9,17 +9,25 @@ def validate_in_range(user_input):
 
 def main():
     # simple python guessing number game
-    user_number = int(input("Can you guess the number between 1 to 10? "))
+    user_input = input("Can you guess the number between 1 to 10 (you have 3 tries)? 'Q' to quit! ")
     user_tries = 1
     python_guessed = random.randint(1, 10)
 
-    while user_tries != 3:
-        if user_number != python_guessed:
+    while user_input.upper() != 'Q':
+        if user_tries == 3:
+            break
+
+        user_input = int(user_input)
+        if user_input != python_guessed:
             user_tries += 1
-            user_number = int(input("Try again please: "))
+            user_input = input("Try again please: ")
         else:
             print("You guessed the right number", python_guessed, sep="")
 
-    print("Sorry you've run out of tries, the number is:", python_guessed)
+    # when number guessed then issue here
+    if user_input.upper() == 'Q':
+        print("You've quit! :-(")
+    else:
+        print("Sorry you've run out of tries, the number is:", python_guessed)
 
 main()
