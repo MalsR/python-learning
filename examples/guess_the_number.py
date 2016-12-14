39fb5781d3ceb23ba3fc2785b89dc46c9faa219e
MalsR <malinda_rajapakse@hotmail.com>
2016-12-14 21:18:48 +0000

    Move guess the number login to invoke on python main()

--- examples/guess_the_number.py
+++ examples/guess_the_number.py
@@ -1,21 +1,25 @@
 import random
 
-# simple python guessing number game
-user_number = int(input("Can you guess the number between 1 to 10? "))
-user_tries = 1
-python_guessed = random.randint(1, 10)
-
-while user_tries != 3:
-    if user_number != python_guessed:
-        user_tries += 1
-        user_number = int(input("Try again please: "))
-    else:
-        print("You guessed the right number", python_guessed, sep="")
-
-print("Sorry you've run out of tries, the number is:", python_guessed)
-
 
 # validate user input
 def validate_in_range(user_input):
     if user_input > 10:
         print("Please enter a number less than ten")
+
+
+def main():
+    # simple python guessing number game
+    user_number = int(input("Can you guess the number between 1 to 10? "))
+    user_tries = 1
+    python_guessed = random.randint(1, 10)
+
+    while user_tries != 3:
+        if user_number != python_guessed:
+            user_tries += 1
+            user_number = int(input("Try again please: "))
+        else:
+            print("You guessed the right number", python_guessed, sep="")
+
+    print("Sorry you've run out of tries, the number is:", python_guessed)
+
+main()
